a7ad62c6dba0204c8e63d1777e52ca6eca4a092f
nicholas-delarosa <nicholasandresdelarosarivera@hotmail.com>
2026-03-11 10:10:34 -0500

    Se implementó el calculo del costo_total multiplicando precio por cantidad (TASK 3)

--- inventario.py
+++ inventario.py
@@ -1,22 +1,21 @@
 # Solicitar el nombre del producto
 ask_nombre = input("Ingrese el nombre del producto: ")
 
-# Validar que el precio sea un número decimal válido
+# Validar precio
 while True:
     try:
-        # Solicitar el precio del producto
         ask_precio = float(input("Ingrese el precio del producto: $"))
         break
     except ValueError:
-        # Mensaje de error si el valor no es válido
         print("Error: Por favor, ingrese un número decimal válido (ejemplo: 10.5).")
 
-# Validar que la cantidad sea un número entero válido
+# Validar cantidad
 while True:
     try:
-        # Solicitar la cantidad del producto
         ask_cantidad = int(input("Ingrese la cantidad del producto: "))
         break
     except ValueError:
-        # Mensaje de error si el valor no es válido
         print("Error: Por favor, ingrese un número entero válido.")
+
+# Calcular el costo total del producto
+costo_total = ask_precio * ask_cantidad
